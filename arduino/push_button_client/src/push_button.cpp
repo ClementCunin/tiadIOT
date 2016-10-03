6e5cdd69796785b54f77af0c6a5b28da460ff4d5
Wykiki <jmgaplay@hotmail.fr>
2016-10-03 18:38:03 +0200

    Update broken readme for linux and upload push_button_client

---
+++ arduino/push_button_client/src/push_button.cpp
@@ -0,0 +1,48 @@
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+
+#define BUT1  14
+
+const char*   ssid = "<SSID>";
+const char*   password = "<PWD>";
+const char*   ip = "<IP>";
+WiFiClient    client;
+
+String        status;
+
+void setup() {
+  // init LED pin
+  pinMode(BUT1, INPUT);
+
+  // init Serial
+  Serial.begin(9600);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  // WIFI connection
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+
+  // If a button have been pressed, send fresh values to the server
+  if (digitalRead(BUT1) == 1) {
+    client.connect(ip, 80);
+    status == "ON" ? status = "OFF" : status = "ON";
+    Serial.println("Send to server : ");
+    String  str = String("GET ?LED=") + status;
+    Serial.println(str);
+    client.println(str);
+  }
+
+  delay(200);
+}
